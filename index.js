8920218ea5859d8a63fa56602fb1a1fc28448ee7
William Boehringer <104523477+williamboehringer@users.noreply.github.com>
2023-06-30 13:40:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+document.querySelector('.signup-form').addEventListener('submit', function(event){
+    event.preventDefault()
+  });
+
+document.querySelector('input').addEventListener('keypress', function(event){
+    const error = document.querySelector('.error')
+    const email = document.querySelector('input')
+
+    if (email.value === '' || !email.value.includes('@') || email.value.length < 10) {
+        email.classList.add('invalid')
+        email.classList.remove('valid')
+
+    }
+    else {
+        error.style.display = 'none'
+        email.classList.remove('invalid')
+        email.classList.add('valid')
+    }
+
+})
+
+const handleSubmit = () => {
+    const signup = document.querySelector(".newsletter")
+    const newsletterSuccess = document.querySelector(".newsletter-success")
+    const error = document.querySelector('.error')
+    const email = document.querySelector('input')
+
+    console.log(email)
+
+    if (email.value === '' || !email.value.includes('@') || email.value.length < 10) {
+        error.style.display = 'inline'
+    }
+    else {
+        newsletterSuccess.style.display = 'grid'
+        signup.style.display = 'none'
+    }
+}
+
+const dismiss = () => {
+    const signup = document.querySelector(".newsletter")
+    const newsletterSuccess = document.querySelector(".newsletter-success")
+
+    signup.style.display = 'grid'
+    newsletterSuccess.style.display = 'none'
+}
